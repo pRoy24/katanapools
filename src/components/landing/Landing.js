97c483308cd01a94f133399dd97d405b5424b24d
proy24 <roypritam1234@gmail.com>
2020-03-21 21:59:38 +0000

    revert changes to fallback to infura

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,10 +1,12 @@
+
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Container, Row, Col, Button} from 'react-bootstrap';
-import HelpComponent from '../help/HelpComponent';
+
 import SwapTokensContainer from '../swap/SwapTokensContainer';
 import ExploreTokensContainer from '../explore/ExploreTokensContainer';
 import PoolTokensContainer from '../pool/PoolTokensContainer';
+import HelpComponent from '../help/HelpComponent';
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,35 +24,32 @@ export default class Landing extends Component {
     this.state = {currentView: 'pending'};
   }
   componentWillMount() {
+
+    let web3 = window.web3;
     const self = this;
-    const infura_provider = process.env.REACT_APP_INFURA_PROVIDER;
-
-    if (window.ethereum && window.ethereum.selectedAddress) {
-      const web3 = new Web3(window.ethereum);
-      window.web3 = web3;
-    } else {
-      const web3 = new Web3(new Web3.providers.HttpProvider(`${infura_provider}`))
-      window.web3 = web3;
-    }
-
     window.addEventListener('load', async() => {
       if (window.ethereum) {
-
+        window.web3 = new Web3(window.ethereum);
+        this.props.setMetaskConnected();
         try {
           await window.ethereum.enable();
-          window.web3 = new Web3(window.ethereum);
-          self.props.setMetaskConnected();
+          self.props.setUserEnvironment();
+          this.setState({currentView: 'home'});
         }
         catch (error) {
-         //  this.setState({currentView: 'prompt'});
+           this.setState({currentView: 'prompt'});
         }
       window.ethereum.on('accountsChanged', function (accounts) {
 
         async function reload() {
+          window.web3 = new Web3(window.ethereum);
           try {
+
             await window.ethereum.enable();
-               window.web3 = new Web3(window.ethereum);
-               self.props.setMetaskConnected();
+            self.props.setUserEnvironment();
+            self.setState({currentView: ''}, function(){
+              self.setState({currentView: 'home'});
+            });
           }
           catch (error) {
 
@@ -59,17 +58,30 @@ export default class Landing extends Component {
         reload();
     })
       }
-
+      else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+      }
+      else {
+           this.setState({currentView: 'prompt'});
+      }
     });
-
   }
 
   render() {
 
+    let appHome = <span/>;
+    if (this.state.currentView === 'home') {
+      appHome = <AppHome {...this.props}/>
+    } else if (this.state.currentView === 'prompt'){
+      appHome = <Web3Prompt/>
+    }
+    if (window.web3 && window.web3.currentProvider && window.web3.currentProvider.selectedAddress === null) {
+      return  <div className="install-prompt">Please click connect on Metamask prompt to continue.</div>
+    }
     return (
       <Container>
         <div>
-         <AppHome {...this.props}/>
+          {appHome}
         </div>
       </Container>
       )
@@ -85,23 +97,17 @@ class AppHome extends Component {
     this.web3 = window.web3;
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log('got new props');
+  componentWillMount() {
+
+      //  this.props.getConvertibleToSmartTokenMap();
   }
 
+
   render() {
-    const {user: {providerConnected}} = this.props;
-    let connectionPromptMessage = <span/>;
-    if (!providerConnected) {
-      connectionPromptMessage = (
-      <div className="install-prompt">
-        Please install <a href="https://metamask.io/" target="_blank">Metamask</a> and allow Katana to connect to proceed.
-      </div>
-        )
-    }
+    const web3 = window.web3;
+
     return (
       <div>
-        {connectionPromptMessage}
           <Switch>
           <Route path="/swap">
             <SwapTokensContainer  getAllConvertibleTokens={this.props.getAllConvertibleTokens}
@@ -113,13 +119,14 @@ class AppHome extends Component {
           <Route path="/explore">
             <ExploreTokensContainer getAllConvertibleTokens={this.props.getAllConvertibleTokens}/>
           </Route>
-          <Route path="/help">
-            <HelpComponent/>
-          </Route>
           <Route exact path="/">
             <SwapTokensContainer getAllConvertibleTokens={this.props.getAllConvertibleTokens}
         getAllSmartTokens={this.props.getAllSmartTokens}/>
           </Route>
+          <Route path="/help">
+            <HelpComponent/>
+          </Route>
+
         </Switch>
     </div>
       )
@@ -127,3 +134,12 @@ class AppHome extends Component {
 }
 
 
+class Web3Prompt extends Component {
+  render() {
+    return (
+      <div className="install-prompt">
+        Please install <a href="https://metamask.io/" target="_blank">Metamask</a> and allow Katana to connect to proceed.
+      </div>
+      )
+  }
+}
